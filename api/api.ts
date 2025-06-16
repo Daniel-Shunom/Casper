6e2d1adcc506c6a54421621fe38111f6cf2e2a1e
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-16 17:24:15 -0400

    intial commit

---
+++ api/api.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosRequestHeaders} from 'axios'
+import type { AxiosRequestConfig } from 'axios'
+import * as AxiosLogger from 'axios-logger'
+import * as SecureStore from 'expo-secure-store'
+import { Platform } from 'react-native'
+
+interface AdaptAxiosRequestConfig extends AxiosRequestConfig {
+  headers: AxiosRequestHeaders
+}
+
+function baseURL(): string | undefined {
+  return process.env.EXPO_PUBLIC_BASE_URL
+} 
+
+const api = axios.create({
+  baseURL: baseURL(),
+  timeout: 30000
+})
+
+const getToken = () => {
+  if (Platform.OS === 'web') {
+    return localStorage.getItem(SESSION_TOKEN_KEY)?.trim()
+  }
+  return SecureStore.getItem(SESSION_TOKEN_KEY)?.trim()
+}
+
+api.interceptors.requests.use(
+  (config: AxiosRequestConfig): AdaptAxiosRequestConfig => {
+    const token = getToken()
+
+    if (token && config.headers) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+if (__DEV__) {
+  api.interceptors.request.use(AxiosLogger.requestLogger)
+  api.interceptors.response.use(AxiosLogger.responseLogger)
+}
