a4bb8920331bf4af43cbde6c60806af96dad57c3
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-16 23:46:04 -0400

    formatting?

--- api/types/types.ts
+++ api/types/types.ts
@@ -1,90 +1,95 @@
 type Enumerate<
   N extends number,
   Acc extends number[] = []
-> = Acc['length'] extends N 
-  ? Acc[number] 
-  : Enumerate<N, [...Acc, Acc['length']]>
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
 
-type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
 
 type DOB = {
-  day: IntRange<1, 32>,
-  month: IntRange<1, 13>,
-  year: number
-}
+  day: IntRange<1, 32>;
+  month: IntRange<1, 13>;
+  year: number;
+};
 
-type Gender = 'cisgender'
-  | 'transgender'
-  | 'nonbinary'
-  | 'other'
+type Gender = 
+  |"cisgender" 
+  | "transgender" 
+  | "nonbinary" 
+  | "other";
 
-type Pronouns = `${string}/${string}`
+type Pronouns = `${string}/${string}`;
 
-type UserName = `@${string}`
+type UserName = `@${string}`;
 
 type Name = {
-  first: string,
-  last: string
-}
+  first: string;
+  last: string;
+};
 
 export type NewUser = {
-  name: Name,
-  username: UserName,
-  userid: string,
-  userauth: boolean,
-  userdob: DOB,
-  usergender: Gender,
-  userpronouns: Pronouns
-}
+  name: Name;
+  username: UserName;
+  userid: string;
+  userauth: boolean;
+  userdob: DOB;
+  usergender: Gender;
+  userpronouns: Pronouns;
+};
 
 export type UserSignin = {
-  username: string,
-  password: string
-}
+  username: string;
+  password: string;
+};
 
 export type UserSession = {
-  userid: string
-}
+  userid: string;
+};
 
-export type UserAuth  = {
-  userid: string,
-  authnticated: boolean
-}
+export type UserAuth = {
+  userid: string;
+  authnticated: boolean;
+};
 
 export type UserProfile = {
-  userid: string,
-  name: string,
-  username: string,
-  gender: Gender,
-  pronouns: Pronouns,
-  authenticated: boolean
-}
+  userid: string;
+  name: string;
+  username: string;
+  gender: Gender;
+  pronouns: Pronouns;
+  authenticated: boolean;
+};
 
 // ==========ROOMS========== //
 
-type Capacity = 'small' 
-  | 'medium' 
-  | 'large'
+type Capacity = 
+  | "small" 
+  | "medium" 
+  | "large";
 
-export type NewRoom = { 
-  ownerid: String,
-  roomname: string,
-  roomcapacity: Capacity,
-}
+export type NewRoom = {
+  ownerid: String;
+  roomname: string;
+  roomcapacity: Capacity;
+};
 
 export type DeleteRoom = {
-  userid: string,
-  roomid: string
-}
+  userid: string;
+  roomid: string;
+};
 
 export type NewRoomResponse = {
-  roomname: string,
-  roomid: string
-}
+  roomname: string;
+  roomid: string;
+};
 
 // ==========LOGIN========== //
 
 export type Auth = {
-  username: string,
-  password: string
-}
+  username: string;
+  password: string;
+};
