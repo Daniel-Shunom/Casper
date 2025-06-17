ffa0a07d8aa91d6c95eb34e0b4fc49320811c6eb
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-17 10:17:58 -0400

    updated linter rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,16 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      'no-undef': 'error',
+      'no-unused-vars': 'warn',
+      'eqeqeq': ['error', 'always'],
+      'default-case': 'error',
+      'no-eval': 'error',
+      'max-len': ['warn', { 'code': 100 }],
+      'prefer-const': 'warn',
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'no-var': 'error'
+    }
   },
 ]);
