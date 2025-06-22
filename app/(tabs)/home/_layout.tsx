84f149351f47cfd5dfeca5200f86f65b55e4b453
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-22 14:39:03 -0400

    used proper Svg variant

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,12 +1,9 @@
-import PageLayout from '@/components/ui/layout/PageLayout'; // Your component with the sidebar
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
-export default function HomeLayout() {
+export default () => {
   return (
-    <PageLayout showHeader={true}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    </PageLayout>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 }
