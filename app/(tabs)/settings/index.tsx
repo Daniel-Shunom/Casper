1e8740204b00b992e5c99be2a8a0194a2b4c4338
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-23 15:02:55 -0400

    boilerplate

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,9 +1,19 @@
-import { View } from "react-native";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
 export default () => {
   return (
-    <View>
-      
-    </View>
-  )
-}
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.desc}>Hi brother</Text>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#d9d9d9'
+  },
+  desc: {
+    color: "#fff",
+  },
+});
