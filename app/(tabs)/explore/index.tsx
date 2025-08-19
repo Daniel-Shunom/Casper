07ad7b84b55f90c3f7fe3ea69f1bc422d73fa81e
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-08-18 23:46:52 -0400

    temp component sandbox.

--- app/(tabs)/explore/index.tsx
+++ app/(tabs)/explore/index.tsx
@@ -1,11 +1,26 @@
+import EventCard from "@/components/EventCard";
+import GenreCard from "@/components/GenreCard";
+import TopicCard from "@/components/TopicCard";
 import React from "react";
 import {
   View,
+  StyleSheet,
+  SafeAreaView
 } from "react-native";
 
 export default function DemoScreen() {
   return (
-    <View />
+    <View style={styles.container}>
+      <SafeAreaView />
+      <GenreCard title="demo-genre" />
+      <TopicCard topic="foofighters" />
+      <EventCard event="demp-event" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
