a06cbd135f12aac49e87b269fca04321c05c1a90
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-08-19 03:30:46 -0400

    added gauranteed function

--- ctx/stores/rooms/roomStore.ts
+++ ctx/stores/rooms/roomStore.ts
@@ -4,6 +4,7 @@ interface CentralRoomState {
   roomsmetadata: Array<RoomMetadata>
   setRoomsMetadata: (metadata: RoomMetadata | Array<RoomMetadata>) => void
   getRoomMetadata: (roomid: string) => Option<RoomMetadata> 
+  getGauranteedRoomMetadata: (roomid: string) => RoomMetadata
 }
 
 export const useCentralRoomStore = zus.create<CentralRoomState>((set, get) => ({
@@ -26,7 +27,21 @@ export const useCentralRoomStore = zus.create<CentralRoomState>((set, get) => ({
     return metadata
     ? { Some: metadata } 
     : { None: null }
-  }
+  },
+
+  getGauranteedRoomMetadata: (id) => {
+    let metadata = get().roomsmetadata.find(metadata => {
+      return metadata.id === id
+    })
+
+    return metadata 
+    ? metadata
+    : {
+      id: "not-an-id",
+      name: "not-a-name",
+      icon: "book"
+    }
+  },
 
 }))
 
@@ -36,4 +51,6 @@ export type RoomMetadata = {
   id: string,
   name: string,
   icon: string,
+  description: string,
+  owner: string
 }
