954ec5b517cebcc50db9b2e9f32bec9ed3395af7
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-08-19 02:01:53 -0400

    setup room store

--- ctx/stores/rooms/roomStore.ts
+++ ctx/stores/rooms/roomStore.ts
@@ -1,1 +1,39 @@
+import * as zus from 'zustand'
 
+interface CentralRoomState {
+  roomsmetadata: Array<RoomMetadata>
+  setRoomsMetadata: (metadata: RoomMetadata | Array<RoomMetadata>) => void
+  getRoomMetadata: (roomid: string) => Option<RoomMetadata> 
+}
+
+export const useCentralRoomStore = zus.create<CentralRoomState>((set, get) => ({
+  roomsmetadata: [],
+
+  setRoomsMetadata: (metadata) => {
+    set(() => {
+      const newMtd = Array.isArray(metadata)
+        ? metadata
+        : [metadata]
+      return { roomsmetadata: newMtd }
+    })
+  },
+
+  getRoomMetadata: (id) => {
+    let metadata = get().roomsmetadata.find(metadata => {
+      return metadata.id === id
+    })
+
+    return metadata
+    ? { Some: metadata } 
+    : { None: null }
+  }
+
+}))
+
+
+//types
+export type RoomMetadata = {
+  id: string,
+  name: string,
+  icon: string,
+}
